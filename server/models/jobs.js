3746a939cb10863307ff0bf410b706826d92efe8
Swati <swatic019@gmail.com>
2023-12-10 00:13:55 +0530

    Removed node_modules folder

---
+++ server/models/jobs.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+
+const jobPostSchema = new mongoose.Schema({
+  companyName: {
+    type: String,
+    required: true,
+  },
+  logoUrl: {
+    type: String,
+    required: true,
+  },
+  jobPosition: {
+    type: String,
+    required: true,
+  },
+  monthlySalary: {
+    type: String,
+    required: true,
+  },
+  jobType: {
+    type: String,
+    enum: ["Full-time", "Part-time", "Internship"],
+    required: true,
+  },
+  remote: {
+    type: String,
+    enum: ["Remote", "Office"],
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  jobDescription: {
+    type: String,
+    required: true,
+  },
+  aboutCompany: {
+    type: String,
+    required: true,
+  },
+  skills: {
+    type: [String],
+    required: true,
+  },
+  information: {
+    type: String,
+    required: ture,
+  },
+});
+
+
+module.exports = mongoose.model('JobPost', jobPostSchema)

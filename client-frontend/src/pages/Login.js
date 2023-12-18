0c822ea2a4be61a2c37bce556faf532aedb5df8a
Swati <swatic019@gmail.com>
2023-12-18 23:38:07 +0530

    login page updated

--- client-frontend/src/pages/Login.js
+++ client-frontend/src/pages/Login.js
@@ -1,8 +1,19 @@
-
-export default function Login(){
-    return(
-        <>
-        <p>login page</p>
-        </>
-    )
+export default function Login() {
+  return (
+    <>
+      <div className="login_container">
+        <div className="login_form_container">
+          <h2>Already have an account?</h2>
+          <p>Your personal job finder is here</p>
+          <input type="email" placeholder="Email"></input>
+          <input type="password" placeholder="Password"></input>
+          <button className="login_btn">Sign in</button>
+          <p className="signup_link_para">Don't have an account? <span className="signup_link">Sign Up</span></p>
+        </div>
+        <div className="login_bg_img_container">
+            
+        </div>
+      </div>
+    </>
+  );
 }

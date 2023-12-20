1c45aa7201d2e75bbed17ddb16c337618a309d9c
Swati <swatic019@gmail.com>
2023-12-20 17:00:11 +0530

    register page css updated

--- client-frontend/src/pages/Login.js
+++ client-frontend/src/pages/Login.js
@@ -1,7 +1,11 @@
+import { useNavigate } from "react-router-dom";
 import login_bgImg from "../images/login_bg_img.png";
 import "./login.css";
 
 export default function Login() {
+  const navigate = useNavigate();
+
+
   return (
     <>
       <div className="login_container">
@@ -9,17 +13,21 @@ export default function Login() {
           <h2>Already have an account?</h2>
           <p className="login_heading">Your personal job finder is here</p>
           <div className="input_wrapper">
-            <input type="email" placeholder="Email"></input>
-            <input type="password" placeholder="Password"></input>
+            <input type="email" placeholder="Email" className="login_input"></input>
+            <input type="password" placeholder="Password" className="login_input"></input>
           </div>
           <button className="login_btn">Sign in</button>
           <p className="signup_link_para">
-            Don't have an account? <span className="signup_link">Sign Up</span>
+            Don't have an account? <span className="signup_link" onClick={()=>navigate('/register')}>Sign Up</span>
           </p>
         </div>
         <div className="login_bg_img_container">
           <h2 className="tagline">Your Personal Job Finder</h2>
-          <img src={login_bgImg} alt="login_bgImg" className="loginPage_bg"></img>
+          <img
+            src={login_bgImg}
+            alt="login_bgImg"
+            className="loginPage_bg"
+          ></img>
         </div>
       </div>
     </>
--- client-frontend/src/pages/Registration.js
+++ client-frontend/src/pages/Registration.js
@@ -1,10 +1,48 @@
-import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./register.css";
+import login_bgImg from "../images/login_bg_img.png";
 
 export default function Registration() {
+  const navigate = useNavigate();
+
   return (
     <>
-      <div className="container">
-       
+      <div className="registerPage_container">
+        <div className="register_form_container">
+          <h2>Create an account</h2>
+          <p className="login_heading">Your personal job finder is here</p>
+          <div className="input_wrapper">
+            <input type="text" placeholder="Name" className="input"></input>
+            <input type="email" placeholder="Email" className="input"></input>
+            <input type="text" placeholder="Mobile" className="input"></input>
+            <input
+              type="password"
+              placeholder="Password"
+              className="input"
+            ></input>
+            <div className="conditions_check_wrapper">
+              <input type="checkbox"></input>
+              <label className="conditions_check">
+                By creating an account, I agree to our terms of use and privacy
+              </label>
+            </div>
+          </div>
+          <button className="register_btn">Create Account</button>
+          <p className="signup_link_para">
+            Already have an account?
+            <span className="signup_link" onClick={() => navigate("/")}>
+              Sign In
+            </span>
+          </p>
+        </div>
+        <div className="login_bg_img_container">
+          <h2 className="tagline">Your Personal Job Finder</h2>
+          <img
+            src={login_bgImg}
+            alt="login_bgImg"
+            className="loginPage_bg"
+          ></img>
+        </div>
       </div>
     </>
   );

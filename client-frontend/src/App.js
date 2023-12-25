8f2d2311c967d07c3d251303d185c6b930f62c5d
Swati <swatic019@gmail.com>
2023-12-25 12:20:52 +0530

    login page updated

--- client-frontend/src/App.js
+++ client-frontend/src/App.js
@@ -2,14 +2,14 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Registration from "./pages/RegistrationPage.js";
 import Login from "./pages/LoginPage.js";
-import Home from "./pages/home_without_login/Home.js"
+import Home from "./pages/home_without_login/Home.js";
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-        <Route path="/" element={<Home />}></Route>
+          <Route path="/" element={<Home />}></Route>
           <Route path="/login" element={<Login />}></Route>
           <Route path="/register" element={<Registration />}></Route>
         </Routes>

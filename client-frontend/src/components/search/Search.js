e8a6cec4ee3c06c04e3ae003dd1f6986a7a5280f
Swati <swatic019@gmail.com>
2024-01-02 19:36:32 +0530

    add job page css updated

--- client-frontend/src/components/search/Search.js
+++ client-frontend/src/components/search/Search.js
@@ -1,14 +1,51 @@
 import searchbar from "../../images/search.png";
 import closeIcon from "../../images/close_icon.png";
 import styles from "./Search.module.css";
+import { createJob } from "../../apis/jobs";
+import { useNavigate } from "react-router-dom";
 
 export default function Search({ isLoggedIn }) {
+  const navigate = useNavigate();
+  function addJob(
+    companyName,
+    logoUrl,
+    jobPosition,
+    monthlySalary,
+    jobType,
+    remote,
+    location,
+    jobDescription,
+    aboutCompany,
+    skills,
+    information
+  ) {
+    try {
+      createJob(
+        companyName,
+        logoUrl,
+        jobPosition,
+        monthlySalary,
+        jobType,
+        remote,
+        location,
+        jobDescription,
+        aboutCompany,
+        skills,
+        information
+      );
+      navigate("add-job");
+    } catch (error) {}
+  }
   return (
     <>
       {isLoggedIn ? (
         <div className={styles.search_container}>
           <div className={styles.input_container}>
-            <img src={searchbar} alt="searchbar" className={styles.searchbar}></img>
+            <img
+              src={searchbar}
+              alt="searchbar"
+              className={styles.searchbar}
+            ></img>
             <input type="text" placeholder="Type any job title"></input>
           </div>
           <div className={styles.searchbySkills_clear_container}>
@@ -35,14 +72,20 @@ export default function Search({ isLoggedIn }) {
                 </div>
               </div>
             </div>
-            <button className={styles.addJob_btn}>+Add Job</button>
+            <button onClick={addJob} className={styles.addJob_btn}>
+              +Add Job
+            </button>
           </div>
           <p className={styles.clear_skills}>Clear</p>
         </div>
       ) : (
         <div className={styles.search_container}>
           <div className={styles.input_container}>
-            <img src={searchbar} alt="searchbar" className={styles.searchbar}></img>
+            <img
+              src={searchbar}
+              alt="searchbar"
+              className={styles.searchbar}
+            ></img>
             <input type="text" placeholder="Type any job title"></input>
           </div>
           <div className={styles.searchbySkills_clear_container}>

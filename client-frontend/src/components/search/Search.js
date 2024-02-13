fbed0063ebac73d14bada0a0bd6bde35ffc8c448
Swati <swatic019@gmail.com>
2024-02-13 18:39:50 +0530

    search by skills updated

--- client-frontend/src/components/search/Search.js
+++ client-frontend/src/components/search/Search.js
@@ -3,9 +3,28 @@ import closeIcon from "../../images/close_icon.png";
 import styles from "./Search.module.css";
 import { createJob } from "../../apis/jobs";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export default function Search({ isLoggedIn }) {
   const navigate = useNavigate();
+  const [selectedSkills, setSelectedSkills] = useState([]);
+
+  function handleChange(e) {
+    const { value } = e.target;
+    setSelectedSkills((prevSkills) => {
+      if (!prevSkills.includes(value)) {
+        if (prevSkills.length < 6) {
+          return [...prevSkills, value];
+        }
+        return prevSkills;
+      } else {
+        return prevSkills.filter((skill) => skill !== value);
+      }
+    });
+  }
+
+  console.log(selectedSkills);
+
   function addJob(
     companyName,
     logoUrl,
@@ -49,27 +68,56 @@ export default function Search({ isLoggedIn }) {
             <input type="text" placeholder="Type any job title"></input>
           </div>
           <div className={styles.searchbySkills_clear_container}>
-            <div className={styles.searchbySkills_wrapper}>
+            <div
+              className={styles.searchbySkills_wrapper}
+              value={selectedSkills}
+              onChange={handleChange}
+              name="jobType"
+            >
               <select className={styles.select}>
-                <option value="skillsRequired">Skills</option>
+                <option value="React.js">React.js</option>
+                <option value="Angular">Angular</option>
+                <option value="Vue.js">Vue.js</option>
+                <option value="Express.js">Express.js</option>
+                <option value="Django">Django</option>
+                <option value="Flask">Flask</option>
+                <option value="Spring Boot">Spring Boot</option>
+                <option value="Ruby on Rails">Ruby on Rails</option>
+                <option value="Laravel">Laravel</option>
+                <option value="ASP.NET Core">ASP.NET Core</option>
+                <option value="HTML">HTML</option>
+                <option value="CSS">CSS</option>
+                <option value="Javascript">Javascript</option>
+                <option value="Python">Python</option>
+                <option value="Java">Java</option>
+                <option value="C##">C##</option>
+                <option value="C++">C++</option>
+                <option value="Ruby">Ruby</option>
+                <option value="Swift">Swift</option>
+                <option value="Cybersecurity">Cybersecurity</option>
+                <option value="Typescript">Typescript</option>
+                <option value="PHP">PHP</option>
+                <option value="Rust">Rust</option>
+                <option value="Dart">Dart</option>
+                <option value="RESTful APIs">RESTful APIs</option>
+                <option value="GraphQL">GraphQL</option>
+                <option value="HTML">HTML</option>
+                <option value="Git">Git</option>
+                <option value="Data Structures and Algorithms">
+                  Data Structures and Algorithms
+                </option>
               </select>
               <div className={styles.skills_wrapper}>
-                <div className={styles.skill_withClose_div}>
-                  <div className={styles.skill_div}>Frontend</div>
-                  <img
-                    src={closeIcon}
-                    alt="closeIcon"
-                    className={styles.closeIcon}
-                  ></img>
-                </div>
-                <div className={styles.skill_withClose_div}>
-                  <div className={styles.skill_div}>CSS</div>
-                  <img
-                    src={closeIcon}
-                    alt="closeIcon"
-                    className={styles.closeIcon}
-                  ></img>
-                </div>
+                {selectedSkills.map((skill) => (
+                  <div className={styles.skill_withClose_div}>
+                    <div className={styles.skill_div}>{skill}</div>
+                    <img
+                      src={closeIcon}
+                      alt="closeIcon"
+                      className={styles.closeIcon}
+                    ></img>
+                  </div>
+                ))}
               </div>
             </div>
             <button onClick={addJob} className={styles.addJob_btn}>

fcaa2fbb080a250746f424c441ac6abdf8626b25
Swati <swatic019@gmail.com>
2024-02-26 18:20:10 +0530

    loader updated

--- client-frontend/src/pages/home_page/Home.js
+++ client-frontend/src/pages/home_page/Home.js
@@ -2,14 +2,7 @@ import "./home.css";
 import Navbar from "../../components/navbar/Navbar";
 import Search from "../../components/search/Search";
 import Card from "../../components/jobs/JobCards";
-import { CSSProperties } from "react";
-import ClipLoader from "react-spinners/ClipLoader";
-
-const override: CSSProperties = {
-  display: "block",
-  margin: "0 auto",
-  borderColor: "red",
-};
+import { ColorRing } from "react-loader-spinner";
 
 export default function Home({
   userData,
@@ -33,13 +26,14 @@ export default function Home({
         {jobsData.length === 0 && !isLoggedIn ? (
           <div className="noJobs_content">No Jobs are added.</div>
         ) : isLoading ? (
-          <ClipLoader
-            color={"#000000"}
-            loading={isLoading}
-            cssOverride={override}
-            size={150}
-            aria-label="Loading Spinner"
-            data-testid="FadeLoader"
+          <ColorRing
+            visible={true}
+            height="80"
+            width="80"
+            ariaLabel="color-ring-loading"
+            wrapperStyle={{}}
+            wrapperClass="color-ring-wrapper"
+            colors={["#e15b64", "#f47e60", "#f8b26a", "#abbd81", "#849b87"]}
           />
         ) : (
           <div className="mainPage_container">
